86cf2d24920b6922683e981a97363fdc611b8f1d
Alvaro Ordonez <alvaro_ordonez319@yahoo.com>
2025-10-06 09:11:40 -0400

    [Feature] Update Project Settings

--- AIChatApp/AIChatAppApp.swift
+++ AIChatApp/AIChatAppApp.swift
@@ -5,6 +5,11 @@
 //  Created by Alvaro Ordonez on 10/6/25.
 //
 
+/*
+ Click on project folder(top most folder in Navigation Pane, select Build Settings, search for
+ Swift Language and update the language settings from 5 to 6.
+ */
+
 import SwiftUI
 
 @main
