12a71d045974e45f1302a16157b6c1378e1c6eda
Alvaro Ordonez <alvaro_ordonez319@yahoo.com>
2025-10-06 08:57:32 -0400

    Initial Commit

---
+++ AIChatApp/AIChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatAppApp.swift
+//  AIChatApp
+//
+//  Created by Alvaro Ordonez on 10/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatAppTests/AIChatAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatAppTests.swift
+//  AIChatAppTests
+//
+//  Created by Alvaro Ordonez on 10/6/25.
+//
+
+import Testing
+@testable import AIChatApp
+
+struct AIChatAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
