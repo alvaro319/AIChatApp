c057a7506b434ff975afad585aaf97f338c75041
Alvaro Ordonez <alvaro_ordonez319@yahoo.com>
2025-12-14 15:26:26 -0500

    [Feature] HeroCellView. Also added miscellaneous corrections in comments for all three Onboarding Views and Welcome View. Added .environment(AppState()) in the Previews for TabBarView and ProfileView so Preview screens wouldn't crash anynmore.

---
+++ AIChatApp/Core/Components/Views/HeroCellView.swift
@@ -0,0 +1,18 @@
+//
+//  HeroCellView.swift
+//  AIChatApp
+//
+//  Created by Alvaro Ordonez on 12/14/25.
+//
+
+import SwiftUI
+
+struct HeroCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeroCellView()
+}
